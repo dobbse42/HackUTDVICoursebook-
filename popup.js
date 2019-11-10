171a1e12e249729d3ef2a7707ddc46f0a2602dc0
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2019-11-10 00:05:09 -0600

    Add files via upload

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ butt.onclick = function(element) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.executeScript(
 			tabs[0].id,
-			{code: 'document.body.page-middle.page-content.var found=false;for(var i=0;((document.getElementById("r-"+i)).length() > 0 && !found);i++){if(document.getElementById("r-"+i+"child").length()>0{found=true}document.write(i);document.write(found);}'
-		}));
-	};
-}
+			{code: 'document.body.page-middle.page-content.var found=false;for(var i=0;((document.getElementById("r-"+i)).length() > 0 && !found);i++){if(document.getElementById("r-"+i+"child").length()>0{found=true}document.write(i);document.write(found);'});
+	});
+};
+//
