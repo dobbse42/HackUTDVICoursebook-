18321384dbf035e5340b90538f3bb52e2abf8518
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2019-11-10 02:49:01 -0600

    Add files via upload

--- rContentScript.js
+++ rContentScript.js
@@ -3,7 +3,38 @@ for(i = 1; ((document.getElementById("r-"+i)) != null);i++)
 	{
 	if((document.getElementById("r-"+i+"child"))!=null){x = i-1}
 	}
+	
+//+3 is prof, + 1 is course number
 const columnTags = document.getElementsByTagName('td');
 for(i = 1; i < columnTags.length; i++) {
-	if(i == (x*7)+3) {document.write(columnTags[i])}
+	if(i == (x*7)+3) {y = (columnTags[i].innerHTML)}
 }
+z = y.split("")
+lank = true;
+b=0;
+w=[];
+for(i = 0; i < z.length; i++) {
+	
+	if(!lank){
+		w[b]=(z[i]);
+		b++;
+	}
+	if(z[i] == '>')
+	{
+		lank = false;
+	}
+	if(lank == false && z[i] == '<')
+	{
+		lank = true;
+	}
+}
+w.pop();
+w.pop();
+for(j = 0; j < w.length; j++) {
+	if(w[j] == " ")
+	{
+		w[j] = "+";
+	}
+} //this works.
+
+window.open("https://www.ratemyprofessors.com/search.jsp?query=utd+"+w.join(""));
